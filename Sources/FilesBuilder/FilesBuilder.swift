467252527df2dc2ae7bbdc404a87499d793a0a84
Woody Melling <woodymelling@gmail.com>
2024-08-29 21:44:58 -0700

    cleanup

--- Sources/FilesBuilder/FilesBuilder.swift
+++ Sources/FilesBuilder/FilesBuilder.swift
@@ -2,38 +2,21 @@
 // https://docs.swift.org/swift-book
 
 import Foundation
-import SwiftUI
-
 
 @resultBuilder
 struct FileSystemBuilder {
     public static func buildBlock<each Content>(_ content: repeat each Content) -> TupleFileSystemComponent<(repeat each Content)> where repeat each Content: FileSystemComponent {
-            return TupleFileSystemComponent((repeat each content))
-        }
+        return TupleFileSystemComponent((repeat each content))
+    }
 }
 
 
 struct TupleFileSystemComponent<T> : FileSystemComponent {
-
     public var value: T
     @inlinable public init(_ value: T) { self.value = value }
-
-//
-//    /// The type of view representing the body of this view.
-//    ///
-//    /// When you create a custom view, Swift infers this type from your
-//    /// implementation of the required ``View/body-swift.property`` property.
-//    @available(iOS 13.0, tvOS 13.0, watchOS 6.0, macOS 10.15, *)
-//    public typealias Body = Never
-//
-//    var body: Never
 }
 
-protocol FileSystemComponent {
-//    associatedtype Body
-//    @FileLoaderBuilder var body: Self.Body { get }
-
-}
+protocol FileSystemComponent { }
 
 struct FileDescriptor {
     var fileName: String
@@ -63,35 +46,3 @@ struct Many: FileSystemComponent {
         self.content = content
     }
 }
-
-// Usage example:
-
-
-
-/*
- struct FileLoader {
-     var body: some FileLoader {
-         File("event-info.yaml") {
-             EventInfoParser()
-         }
-
-         File("contact-info.yaml") {
-             ContactInfoParser()
-         }
-
-         Directory("schedules") {
-             ForEach { fileName in
-                 ScheduleParser()
-             }
-         }
-
-         Directory("artists") {
-             ForEach { fileName in
-                 ArtistParser()
-             }
-         }
-
-     }
- }
-
- */
