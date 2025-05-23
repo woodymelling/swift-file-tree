663f7200b6672b74ea40e3624c450862b90a8c15
Woodrow Melling <woodymelling@gmail.com>
2025-05-23 15:40:10 -0700

    Downgrade minimum version to iOS 17

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "swift-file-tree",
-    platforms: [.macOS(.v15), .iOS(.v18)],
+    platforms: [.macOS(.v15), .iOS(.v17)],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(name: "FileTree", targets: ["FileTree"]),
